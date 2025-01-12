00601f0ca3086927e508da7b9c0df786249b04ac
AlexBuseDragomir <alexandru.buse.dragomir@gmail.com>
2025-01-12 18:40:01 +0200

    Adding the maximum depth of binary tree

---
+++ Maximum depth of binary tree/SolutionRecursive.java
@@ -0,0 +1,14 @@
+class SolutionRecursive {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+    }
+}

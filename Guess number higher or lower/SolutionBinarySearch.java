d00e7858fba16ecfd51d76d249898d3d2c963b17
AlexBuseDragomir <alexandru.buse.dragomir@gmail.com>
2025-01-27 08:51:32 +0200

    Add guess number higher or lower

---
+++ Guess number higher or lower/SolutionBinarySearch.java
@@ -0,0 +1,33 @@
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class SolutionBinarySearch extends GuessGame {
+
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+
+        while (start <= end) {
+            int middle = start + (end - start) / 2;
+            int guess = guess(middle);
+
+            if (guess == -1) {
+                end = middle - 1;
+            } else if (guess == 1) {
+                start = middle + 1;
+            }
+
+            if (guess == 0) {
+                return middle;
+            }
+        }
+
+        return -1;
+    }
+}
